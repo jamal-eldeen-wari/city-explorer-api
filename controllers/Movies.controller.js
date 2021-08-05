c8049dc8ec648f72a9db712e71e0e82a5574aa2b
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-08-05 20:12:43 +0300

    adding cache to my code to improve performance

--- controllers/Movies.controller.js
+++ controllers/Movies.controller.js
@@ -3,13 +3,58 @@ const axios = require('axios');
 const Movie= require('../models/Movie');
 const MOVIE_API_URL = process.env.MOVIE_API_URL;
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const Cache = require('../helpers/cache.helpers');
+let cacheObjMov = new Cache();
+
+async function movieData(city){
+  const queryParams = {
+    params: {
+      api_key: MOVIE_API_KEY,
+      query: city,
+    }
+  };
+  const responseMovie = await axios.get(MOVIE_API_URL,queryParams);
+  const data = responseMovie.data.results.map(element => new Movie(element));
+  cacheObjMov.movies.push({
+    query:city,
+    results:data
+  });
+
+  return data;
+
+}
+
 
 async function getMoviesData (req,res) {
   const city = req.query.city;
-  const responseMovie = await axios.get(`${MOVIE_API_URL}?api_key=${MOVIE_API_KEY}&query=${city}`);
-  console.log(responseMovie.data.results);
-  const data = responseMovie.data.results.map(element => new Movie(element));
-  res.json(data);
+  if (((Date.now() - cacheObjMov.timeStamp) > 86400000)) {
+    // 86400000 is equivalent to 1 day; if exceded reset cache
+    cacheObjMov = new Cache();
+  }
+  if (cacheObjMov.movies.length) {
+
+    const filteredData = cacheObjMov.movies.find((location) => {
+      return location.query === city ;
+    });
+    if (filteredData) {
+      // this (if) is for checking if we have data on the cache if that is the case take from the cache otherwise from the api (else)
+      res.json(filteredData.results);
+    } else {
+      // if no lat or lon match get the data from weather-bit
+      res.json(await movieData(city));
+    }
+  } else {
+
+    res.json(await movieData(city));
+  }
 
 }
+// async function getMoviesData (req,res) {
+//   const city = req.query.city;
+//   const responseMovie = await axios.get(`${MOVIE_API_URL}?api_key=${MOVIE_API_KEY}&query=${city}`);
+//   console.log(responseMovie.data.results);
+//   const data = responseMovie.data.results.map(element => new Movie(element));
+//   res.json(data);
+
+// }
 module.exports = getMoviesData;
--- controllers/Weather.controller.js
+++ controllers/Weather.controller.js
@@ -3,21 +3,54 @@ const axios = require('axios');
 const WEATHERBIT_KEY = process.env.WEATHERBIT_KEY;
 const WEATHERBIT_URL = process.env.WEATHERBIT_URL;
 const Weather= require('../models/Weather');
+const Cache = require('../helpers/cache.helpers');
+let cacheObj = new Cache();
+
+async function weatherData(latitude,longitude){
+  const queryParam = {
+    params: {
+      key: WEATHERBIT_KEY,
+      lat: latitude,
+      lon: longitude,
+    }
+  };
+  const response = await axios.get(WEATHERBIT_URL,queryParam);
+  const data = response.data.data.map(element => new Weather(element));
+
+  cacheObj.forecast.push({
+    lat:latitude,
+    lon:longitude,
+    data:data
+  });
+  return data;
+}
 
 async function getWeatherData(req,res){
   const {latitude,longitude} = req.query;
-  const queryParam={
-    params:{
-      key:WEATHERBIT_KEY,
-      lat:latitude,
-      lon:longitude,
+  if (((Date.now() - cacheObj.timeStamp) > 86400000)) {
+    // 86400000 is equivalent to 1 day; if exceded reset cache
+    cacheObj = new Cache();
+  }
+  if (cacheObj.forecast.length) {
+
+    const filteredData = cacheObj.forecast.find((location) => {
+      return location.lat === latitude && location.lon === longitude;
+    });
+
+    if (filteredData) {
+      console.log('getting the data from the cache');
+      // this (if) is for checking if we have data on the cache if that is the case take from the cache otherwise from the api (else)
+      res.json(filteredData.data);
+    } else {
+      // if no lat or lon match get the data from weather-bit
+      res.json(await weatherData(latitude, longitude));
     }
-  };
-  console.log(WEATHERBIT_URL);
-  console.log(queryParam);
-  const response = await axios.get(WEATHERBIT_URL,queryParam);
-  const data = response.data.data.map(element => new Weather(element));
-  res.json(data);
+  } else {
+
+    res.json(await weatherData(latitude, longitude));
+  }
+  // console.log(WEATHERBIT_URL);
+  // console.log(queryParam);
 
 }
 module.exports = getWeatherData;
